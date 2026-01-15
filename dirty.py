07abad88bc79d6a1d926095eb94f825d4b81d1fd
leovanbon <h4rmonicd1vision@gmail.com>
2026-01-15 14:00:54 +0700

    orororro

---
+++ dirty.py
@@ -0,0 +1,21 @@
+import re
+def x0r(match):
+    val1 = int(match.group(1))
+    val2 = int(match.group(2))
+    return str(val1 ^ val2)
+
+with open("fuckass.py", "r") as fuck:
+    content = fuck.read()
+
+pat = re.compile(r'xor\((\d+),\s*(\d+)\)')
+
+prev = ""
+
+while cur != prev:
+    prev = cur
+    cur = pat.sub(x0r, cur)
+
+print(cur)
+
+with open("xored.py", "w") as f:
+    f.write(cur)
